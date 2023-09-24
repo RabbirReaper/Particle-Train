89b5ee0fc6f0198b1bcf0621b816cd3acb4fa810
RabbirReaper <abc0979999412@gmail.com>
2023-09-24 14:29:54 +0800

    add Sprite

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour{
+    [SerializeField] private int speed;
+    
+    private void Update() {
+        
+    }
+
+}
